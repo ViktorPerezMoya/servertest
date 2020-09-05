1977944261987cae90b3b8403c478386b271c3cd
Victor Ariel Perez <victor.ariel.perez@gmail.com>
2020-09-05 16:55:48 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 3000
+const hbs = require('hbs')
+
+require('./helpers/helpers');
+
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname +'/public'));
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.get('/', (req, res) => {
+  res.render('index',{ nombre: 'VICtor perez mOya'});
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
+
+app.get('/users',(req, res) =>{
+    res.send('Listado de usuarios');
+})
+
+app.use((req,res,next) => {
+    res.status(404).send("Error 404");
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
